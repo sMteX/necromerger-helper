f769ef466404b970464e570496db28a227f92ad6
Lukáš Stuchlík <lukas.stuchlik@prorocketeers.com>
2026-06-22 00:29:16 +0200

    adjustments to rune tab

--- internal/tui/tasks/prestigeplan/tabs/runes/view.go
+++ internal/tui/tasks/prestigeplan/tabs/runes/view.go
@@ -2,6 +2,7 @@ package runes
 
 import (
 	"image/color"
+	"math"
 	"strings"
 
 	"charm.land/lipgloss/v2"
@@ -24,8 +25,13 @@ var (
 
 func (m *Model) View() string {
 	return lipgloss.JoinVertical(lipgloss.Left,
-		lipgloss.NewStyle().Bold(true).Render(
-			runeColumn.Render("Rune")+valueColumn.Render("Have")+valueColumn.Render("Total")+valueColumn.Render("Need"),
+		lipgloss.JoinHorizontal(lipgloss.Top,
+			runeColumn.Bold(true).Render("Rune"),
+			valueColumn.Bold(true).Render("Have"),
+			valueColumn.Bold(true).Render("Total"),
+			lipgloss.NewStyle().Bold(true).Foreground(shared.Colors.Bad).Render("Need"),
+			"/",
+			lipgloss.NewStyle().Bold(true).Foreground(shared.Colors.Good).Render("Excess"),
 		),
 		strings.Repeat("─", runeColumn.GetWidth()+3*valueColumn.GetWidth()),
 		m.renderRuneRow(fieldIce),
@@ -40,10 +46,10 @@ func (m *Model) View() string {
 func (m *Model) renderRuneRow(i fieldIndex) string {
 	r := runeByFieldType[i]
 
-	needRunes := m.result.RuneNeeded[r]
+	needRunes := m.result.RuneTotal[r] - m.PossessedRunes[r]
 
 	needColumn := func() lipgloss.Style {
-		if needRunes == 0 {
+		if needRunes <= 0 {
 			return valueColumn.Foreground(shared.Colors.Good)
 		}
 		return valueColumn.Foreground(shared.Colors.Bad)
@@ -58,7 +64,7 @@ func (m *Model) renderRuneRow(i fieldIndex) string {
 	return runeColumn.Foreground(runeColorMap[r]).Render(string(r)) +
 		valueText +
 		valueColumn.Render(shared.FormatNumberLong(m.result.RuneTotal[r])) +
-		needColumn.Render(shared.FormatNumberLong(needRunes))
+		needColumn.Render(shared.FormatNumberLong(int(math.Abs(float64(needRunes)))))
 }
 
 func (m *Model) GetHelpItems() []string {
